fdc7d43c9f0c00aec498ad4253571776bb7147db
brennanreamer <64988207+brennanreamer@users.noreply.github.com>
2021-12-06 13:28:48 -0500

    Revert "Add files via upload"

--- PokemonGame/PokemonGame/PokemonGame/Caterpie.cpp
+++
@@ -1,9 +0,0 @@
-#include "Caterpie.h"
-#include <string>
-using std::string;
-Caterpie::Caterpie(int level) : Pokemon("Caterpie", "Bug", level) {
-	moves[0] = Attack("Stun Spore", 20, 15, 90);
-	moves[1] = Attack("Tail Whip", 10, 5, 95);
-}
-Caterpie::Caterpie(string in_name, int level) : Pokemon(in_name, "Bug", level) {
-}
--- PokemonGame/PokemonGame/PokemonGame/Spearow.cpp
+++
@@ -1,9 +0,0 @@
-#include "Spearow.h"
-#include <string>
-using std::string;
-Spearow::Spearow(int level) : Pokemon("Spearow", "Flying", level) {
-	moves[0] = Attack("Gust", 20, 15, 90);
-	moves[1] = Attack("Fly", 60, 40, 85);
-}
-Spearow::Spearow(string in_name, int level) : Pokemon(in_name, "Flying", level) {
-}
